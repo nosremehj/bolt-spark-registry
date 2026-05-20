88e673ffec55ed4f7be4b7d012c9094557dea439
nosremehj <jhemerson1931@gmail.com>
2026-05-20 02:06:50 -0300

    Melhoria do teste unitário, e novo teste para a validação dos campos retornados da API VIACEP

--- src/test/java/com/bolt/tecnhical/challenger/web/dto/EnderecoResponseTest.java
+++ src/test/java/com/bolt/tecnhical/challenger/web/dto/EnderecoResponseTest.java
@@ -20,6 +20,13 @@ class EnderecoResponseTest {
 		EnderecoResponse response = EnderecoResponse.from(endereco);
 
 		assertThat(response.complemento()).isEqualTo("Sem complemento");
+		assertThat(response.unidade()).isEqualTo("Sem unidade");
+		assertThat(response.estado()).isEqualTo("Sem estado");
+		assertThat(response.regiao()).isEqualTo("Sem região");
+		assertThat(response.ibge()).isEqualTo("Sem IBGE");
+		assertThat(response.gia()).isEqualTo("Sem GIA");
+		assertThat(response.ddd()).isEqualTo("Sem DDD");
+		assertThat(response.siafi()).isEqualTo("Sem SIAFI");
 	}
 
 	@Test
@@ -35,6 +42,32 @@ class EnderecoResponseTest {
 		EnderecoResponse response = EnderecoResponse.from(endereco);
 
 		assertThat(response.complemento()).isEqualTo("Apto 101");
+	}
+
+	@Test
+	void deveRetornarCamposDoViaCepQuandoInformados() {
+		Endereco endereco = new Endereco();
+		endereco.setCep("31630900");
+		endereco.setLogradouro("Rodovia Papa João Paulo II");
+		endereco.setBairro("Serra Verde (Venda Nova)");
+		endereco.setCidade("Belo Horizonte");
+		endereco.setUf("MG");
+		endereco.setUnidade("Cidade Administrativa");
+		endereco.setEstado("Minas Gerais");
+		endereco.setRegiao("Sudeste");
+		endereco.setIbge("3106200");
+		endereco.setDdd("31");
+		endereco.setSiafi("4123");
+
+		EnderecoResponse response = EnderecoResponse.from(endereco);
+
+		assertThat(response.unidade()).isEqualTo("Cidade Administrativa");
+		assertThat(response.estado()).isEqualTo("Minas Gerais");
+		assertThat(response.regiao()).isEqualTo("Sudeste");
+		assertThat(response.ibge()).isEqualTo("3106200");
+		assertThat(response.ddd()).isEqualTo("31");
+		assertThat(response.siafi()).isEqualTo("4123");
+		assertThat(response.gia()).isEqualTo("Sem GIA");
 	}
 
 }
